b50ad7420973cfaa928004d4711d2e29a0f4e3c6
cheetah676 <league@WorthyMini-6.attlocal.net>
2017-04-17 18:28:15 -0700

    hi

--- src/MyGui.java
+++ src/MyGui.java
@@ -1,15 +1,35 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MyGui {
 	JFrame door;
+	JFrame window;
 	JPanel panel;
-
+	JButton clicker;
+	JButton button;
 	void setup() {
 		door = new JFrame();
 		door.setVisible(true);
 		door.setSize(1000, 1000);
 		panel = new JPanel();
 		door.add(panel);
+		clicker=new JButton("Trump is an idiot!");
+		panel.add(clicker);
+		clicker.addActionListener((e)->{
+			JOptionPane.showMessageDialog(null, "You are amazing!");
+		});
+		window = new JFrame();
+		window.setVisible(true);
+		window.setSize(1000, 1000);
+		panel = new JPanel();
+		window.add(panel);
+		button=new JButton("Trump is an smart!");
+		panel.add(clicker);
+		button.addActionListener((e)->{
+			JOptionPane.showMessageDialog(null, "You suck!");
+		});
+			
 	}
 }
--- src/life.java
+++ src/life.java
@@ -2,7 +2,36 @@ import javax.swing.JOptionPane;
 
 public class life {
 	public static void main(String[] args) {
-		JOptionPane.showInputDialog("What is your real name");
-
+		String name=JOptionPane.showInputDialog("What is your real name");
+		if(name.equalsIgnoreCase("jake")){
+	String password=JOptionPane.showInputDialog("You are amazing");
+	if(password.equalsIgnoreCase("dump trump")){
+		JOptionPane.showMessageDialog(null, "You are a VIP");
+	}
+		}
+		else if(name.equalsIgnoreCase("dave")){
+	String password=JOptionPane.showInputDialog("You are an octopus");
+	if(password.equalsIgnoreCase("dump trump")){
+		JOptionPane.showMessageDialog(null, "You are a VIP");
+	}
+		}
+		else if(name.equalsIgnoreCase("kaden")){
+			String password=JOptionPane.showInputDialog("");
+			if(password.equalsIgnoreCase("dump trump")){
+				JOptionPane.showMessageDialog(null, "You are a VIP");
+			}
+				}
+		else if(name.equalsIgnoreCase("")){
+			String password=JOptionPane.showInputDialog("");
+			if(password.equalsIgnoreCase("dump trump")){
+				JOptionPane.showMessageDialog(null, "You are a VIP");
+			}
+				}
+		else if(name.equalsIgnoreCase("")){
+			String password=JOptionPane.showInputDialog("");
+			if(password.equalsIgnoreCase("dump trump")){
+				JOptionPane.showMessageDialog(null, "You are a VIP");
+			}
+				}
 	}
 }
